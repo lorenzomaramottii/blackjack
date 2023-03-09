03bb2965a49356b7c4cae2d5f3b3d91974c7bac7
lorenzomaramottii <114952389+lorenzomaramottii@users.noreply.github.com>
2023-03-09 10:47:54 +0100

    Add files via upload

--- blackjack.js
+++ blackjack.js
@@ -6,7 +6,6 @@ const MIN_START_VALUE = 10
 
 const MAX_PLAYERS = 4
 
-
 let idCounter = 0
 /*
 player= {
@@ -19,31 +18,60 @@ player= {
 */
 
 
-const addPlayer = () => {
+const addPlayer = (position) => {
     //add player to players
     if (players.length < MAX_PLAYERS){
         players.push({
             id: idCounter,
-            name: "Guest" + (players.length + 1),
+            name: "Guest" + (idCounter + 1),
             credits: START_VALUE,
             playingStatus: false,
             plays: []
         })
-        idCounter++
         console.log(players)
+        document.getElementById(`p${position}`).innerHTML = `
+            <div class="plays">
+            </div>
+
+            <div class = "bottom">
+                <div class="buttons">
+                    <button id="hit">Hit</button>
+                    <button id="stand">Stand</button>
+                </div>
+                <div class="info">
+                    <img class = "small_image" src="matitina.png" onclick="changeName(${idCounter})" alt=""><span id="p${idCounter}_name">Player ${position + 1}</span><br>
+                    <span id = "p${idCounter}_cash">CASH: </span><br>
+                    <span id = "p${idCounter}_win">LAST WIN: </span><br>
+                    <img class = "bottomLeft" src ="inter.png" onclick = "deletePlayer(${position}, ${idCounter})"/>
+                </div>
+            </div>
+        `
+        idCounter++
     } else {
         console.log("Reach max number of players")
     }
 }
 
-const changeName = () => {
-    //change players' name
+const changeName = (playerId) => {
+    let newName = prompt("inserisci nuovo nome")
+    document.getElementById(`p${playerId}_name`).innerHTML = newName
 }
 
 const changePlayerStatus = () => {
     
 }
 
-const deletePlayer = () => {
-    
+const deletePlayer = (position, playerId) => {
+    console.log(players)
+    for (let i = 0; i<players.length; i++){
+        if(players[i].id === playerId){
+            players.splice(i, 1)
+            break
+        }
+    }
+    document.getElementById(`p${position}`).innerHTML = `<span onclick="addPlayer(0)">+</span>`
+}
+
+const tester = () => {
+    console.log(players)
 }
