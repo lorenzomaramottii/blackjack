c3f9c94fb11f78cc38a8ec0cfc505f921a77d4a7
Tond28 <71596724+Tond28@users.noreply.github.com>
2023-03-21 22:29:03 +0100

    wip - added some features and fisches in png

--- blackjack.js
+++ blackjack.js
@@ -6,10 +6,13 @@ const MIN_START_VALUE = 10
 
 const MAX_PLAYERS = 4
 
-let idCounter = 0
+const IMAGE_FOLDER = "images"
+
+let PLAYING_GAME = false
+
 /*
 player= {
-    id,
+    position,
     name,
     credits,
     playingStatus,
@@ -17,36 +20,101 @@ player= {
 }
 */
 
+const fishSet = (position, plays) => {
+    return `
+    <img class = "fiches_image" src="${IMAGE_FOLDER}/fiches/fiches_1.png" alt="" onclick="bet(${position}, ${plays}, 1)">
+    <img class = "fiches_image" src="${IMAGE_FOLDER}/fiches/fiches_5.png" alt="" onclick="bet(${position}, ${plays}, 5)">
+    <img class = "fiches_image" src="${IMAGE_FOLDER}/fiches/fiches_25.png" alt="" onclick="bet(${position}, ${plays}, 25)">
+    <img class = "fiches_image" src="${IMAGE_FOLDER}/fiches/fiches_50.png" alt="" onclick="bet(${position}, ${plays}, 50)">
+    <img class = "fiches_image" src="${IMAGE_FOLDER}/fiches/fiches_100.png" alt="" onclick="bet(${position}, ${plays}, 100)">
+    <img class = "fiches_image" src="${IMAGE_FOLDER}/fiches/fiches_500.png" alt="" onclick="bet(${position}, ${plays}, 500)">
+    `
+}
+
+const cards = [
+    {name: "AC", value: 11},
+    {name: "2C", value: 2},
+    {name: "3C", value: 3},
+    {name: "4C", value: 4},
+    {name: "5C", value: 5},
+    {name: "6C", value: 6},
+    {name: "7C", value: 7},
+    {name: "8C", value: 8},
+    {name: "9C", value: 9},
+    {name: "10C", value: 10},
+    {name: "JC", value: 10},
+    {name: "QC", value: 10},
+    {name: "KC", value: 10},
+    {name: "AD", value: 11},
+    {name: "2D", value: 2},
+    {name: "3D", value: 3},
+    {name: "4D", value: 4},
+    {name: "5D", value: 5},
+    {name: "6D", value: 6},
+    {name: "7D", value: 7},
+    {name: "8D", value: 8},
+    {name: "9D", value: 9},
+    {name: "10D", value: 10},
+    {name: "JD", value: 10},
+    {name: "QD", value: 10},
+    {name: "KD", value: 10},
+    {name: "AH", value: 11},
+    {name: "2H", value: 2},
+    {name: "3H", value: 3},
+    {name: "4H", value: 4},
+    {name: "5H", value: 5},
+    {name: "6H", value: 6},
+    {name: "7H", value: 7},
+    {name: "8H", value: 8},
+    {name: "9H", value: 9},
+    {name: "10H", value: 10},
+    {name: "JH", value: 10},
+    {name: "QH", value: 10},
+    {name: "KH", value: 10},
+    {name: "AS", value: 11},
+    {name: "2S", value: 2},
+    {name: "3S", value: 3},
+    {name: "4S", value: 4},
+    {name: "5S", value: 5},
+    {name: "6S", value: 6},
+    {name: "7S", value: 7},
+    {name: "8S", value: 8},
+    {name: "9S", value: 9},
+    {name: "10S", value: 10},
+    {name: "JS", value: 10},
+    {name: "QS", value: 10},
+    {name: "KS", value: 10},
+]
+
+let playingCards = cards.flatMap(el => [el, el, el, el, el, el])
+
 
 const addPlayer = (position) => {
     //add player to players
-    if (players.length < MAX_PLAYERS){
-        players.push({
-            id: idCounter,
-            name: "Guest" + (idCounter + 1),
+    if (players.length <= MAX_PLAYERS){
+        players[position] = {
+            position: position,
+            name: "Guest " + (position + 1),
             credits: START_VALUE,
             playingStatus: false,
             plays: []
-        })
-        console.log(players)
+        }
         document.getElementById(`p${position}`).innerHTML = `
-            <div class="plays">
+            <div class="plays" id = "p${position}_plays">
             </div>
 
             <div class = "bottom">
-                <div class="buttons">
-                    <button id="hit">Hit</button>
-                    <button id="stand">Stand</button>
+                <div class="buttons" id=p${position}_buttons>
+                    <button onclick="startPlayer(${position})">startPlayer</button>
                 </div>
                 <div class="info">
-                    <img class = "small_image" src="matitina.png" onclick="changeName(${idCounter})" alt=""><span id="p${idCounter}_name">Player ${position + 1}</span><br>
-                    <span id = "p${idCounter}_cash">CASH: </span><br>
-                    <span id = "p${idCounter}_win">LAST WIN: </span><br>
-                    <img class = "bottomLeft" src ="inter.png" onclick = "deletePlayer(${position}, ${idCounter})"/>
+                    <img class = "small_image" src="matitina.png" onclick="changeName(${position})" alt=""><span id="p${position}_name">${players[position].name}</span><br>
+                    <span id = "p${position}_cash">CASH: ${players[position].credits}</span><br>
+                    <span id = "p${position}_win">LAST WIN: </span><br>
+                    <img class = "bottomLeft" src ="inter.png" onclick = "deletePlayer(${position})"/>
                 </div>
             </div>
         `
-        idCounter++
     } else {
         console.log("Reach max number of players")
     }
@@ -61,17 +129,119 @@ const changePlayerStatus = () => {
     
 }
 
-const deletePlayer = (position, playerId) => {
-    console.log(players)
-    for (let i = 0; i<players.length; i++){
-        if(players[i].id === playerId){
-            players.splice(i, 1)
-            break
-        }
-    }
-    document.getElementById(`p${position}`).innerHTML = `<span onclick="addPlayer(0)">+</span>`
+const deletePlayer = (position) => {
+    players[position] = {}
+    document.getElementById(`p${position}`).innerHTML = `<span onclick="addPlayer(${position})">+</span>`
 }
 
 const tester = () => {
     console.log(players)
 }
+
+const shuffle = () => {
+    playingCards = cards.flatMap(el => [el, el, el, el, el, el])
+}
+
+const pickCard = () => {
+    if (playingCards.length <= 69){
+        shuffle()
+    }
+    let cardId = Math.floor(Math.random() * playingCards.length)
+    let card = playingCards[cardId]
+    playingCards.splice(cardId, 1)
+    return card
+}
+
+const startPlayer = (position) => {
+    players[position].playingStatus = true
+    players[position].plays.push({
+        bet: 0,
+        cards: [],
+        result: ""
+    }
+    )
+    document.getElementById(`p${position}_plays`).innerHTML += `
+        <div class="play" id="p${position}_play${players[position].plays.length - 1}">
+            <div class="cards" id="p${position}_play${players[position].plays.length - 1}_cards">
+            </div>
+            <div>
+                scommessa attuale: <span id="p${position}_play${players[position].plays.length - 1}_bet">0</span>
+            </div>
+        </div>
+    `
+    document.getElementById(`p${position}_buttons`).innerHTML = fishSet(position, 0)
+    if (!PLAYING_GAME){
+        startGame()
+    }
+}
+
+const startGame = () => {
+    PLAYING_GAME = true
+}
+
+const bet = (position, playId, value) => {
+    if (players[position].credits < value){
+        alert("not enough credits")
+        return
+    }
+    players[position].plays[playId].bet += value
+    players[position].credits -= value
+    document.getElementById(`p${position}_cash`).innerHTML = `CASH: ${players[position].credits}`
+    document.getElementById(`p${position}_play${playId}_bet`).innerHTML = players[position].plays[playId].bet
+
+    
+}
+
+const hit = (position) => {
+    numCartePlayer++
+    sommaPlayer += playingCards[pickCard()].value
+}
+
+const stand = () => {
+    //set playingStatus to false
+}
+
+const dealerTurn = () => {
+    if (sommaDealer < 17){
+        sommaDealer += playingCards[pickCard()].value
+        numCarteDealer++
+        dealerTurn()
+    }
+    else{
+        return arrDealer = [sommaDealer, numCarteDealer]
+    }
+}
+
+const check = (position , sommaPlayer , numCartePlayer , arrDealer) => {
+    if (players[position].playingStatus){
+        if (sommaPlayer > 21){
+            console.log("BUST")
+        }
+        else if (sommaPlayer == 21 || numCartePlayer == 2){
+            console.log("BLACKJACK")
+        }
+        else if (sommaPlayer < 21 || sommmaPlayer > arrDealer[0]){
+            console.log("WIN")
+        }
+        else if (sommaPlayer == arrDealer[0]){
+            console.log("PUSH")
+        }
+        else if (sommaPlayer < arrDealer[0]){
+            console.log("LOSE")
+        }
+        else if (arrDealer[0] > 21){
+            console.log("DEALER BUST")
+            if (sommaPlayer < 21){
+                console.log("WIN")
+            }
+            else{
+                console.log("LOSE")
+            }
+        }
+        else if(arrDealer[0] == 21 || arrDealer[1] == 2){
+            console.log("LOSE")
+        }
+    }
+}
+
+
