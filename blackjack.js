c829f721e721bc1dc2b16b2707b5dabd3737af69
Tond28 <71596724+Tond28@users.noreply.github.com>
2023-03-28 20:27:38 +0200

    added clock

--- blackjack.js
+++ blackjack.js
@@ -152,6 +152,45 @@ const pickCard = () => {
     return card
 }
 
+function startTimer(duration) {
+
+    document.getElementById("clock").innerHTML = `
+        <svg width="100px" height="100px" viewBox="0 0 42 42" class="donut">
+            <circle id="c1" cx="21" cy="21" r="16" stroke-dasharray="100 0" stroke-dashoffset="100"></circle>
+            <circle id="c2" cx="21" cy="21" r="16" stroke-dasharray="0 100" stroke-dashoffset="0"></circle>
+            <g class="chart-text">
+                <text x="50%" y="50%" dominant-baseline="middle" text-anchor="middle" id="counterText">5</text>
+            </g>
+        </svg>
+        `
+    var timeout = setTimeout(function () {
+        var time = duration;
+        var i = 1;
+        var k = ((i/duration) * 100);
+        var l = 100 - k;
+        i++;
+        document.getElementById("c1").style.strokeDasharray = [l,k];
+        document.getElementById("c2").style.strokeDasharray = [k,l];
+        document.getElementById("c1").style.strokeDashoffset = l;
+        document.getElementById("counterText").innerHTML = duration;
+        var interval = setInterval(function() {
+            if (i > time) {
+                document.getElementById("clock").innerHTML = "Clock out"
+                clearInterval(interval);
+                clearTimeout(timeout);
+                return;
+            }
+            k = ((i/duration) * 100);
+            l = 100 - k;
+            document.getElementById("c1").style.strokeDasharray = [l,k];
+            document.getElementById("c2").style.strokeDasharray = [k,l];
+            document.getElementById("c1").style.strokeDashoffset = l;
+            document.getElementById("counterText").innerHTML = (duration +1)-i;
+            i++;
+        }, 1000);
+    },0);
+}
+
 const startPlayer = (position) => {
     players[position].playingStatus = true
     players[position].plays.push({
@@ -177,6 +216,7 @@ const startPlayer = (position) => {
 
 const startGame = () => {
     PLAYING_GAME = true
+    startTimer(30)
 }
 
 const bet = (position, playId, value) => {
